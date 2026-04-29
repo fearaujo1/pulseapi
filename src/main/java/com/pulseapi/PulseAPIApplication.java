4b81e303436f673335ca320112c731b7ee3ec098
FelipeMGAraujo <168040742+FelipeMGAraujo@users.noreply.github.com>
2026-04-29 10:35:12 -0300

    :sparkles: adiciona estrutura inicial da aplicação Spring Boot

---
+++ src/main/java/com/pulseapi/PulseAPIApplication.java
@@ -0,0 +1,13 @@
+package com.pulseapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PulseAPIApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PulseAPIApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pulseapi/PulseAPIApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pulseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PulseAPIApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
