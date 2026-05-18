b84ed449a7eda1b535e2d694299dc422afebebff
FelipeMGAraujo <168040742+FelipeMGAraujo@users.noreply.github.com>
2026-05-18 14:57:05 -0300

    :sparkles: adiciona entidade e repositório de perfil

---
+++ src/main/java/com/pulseapi/repository/PerfilRepository.java
@@ -0,0 +1,15 @@
+package com.pulseapi.repository;
+
+import com.pulseapi.entity.Perfil;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface PerfilRepository extends JpaRepository<Perfil, Long> {
+
+    Optional<Perfil> findByNome(String nome);
+
+    boolean existsByNome(String nome);
+}
